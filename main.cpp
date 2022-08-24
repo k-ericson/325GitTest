d0530187e83127128dd9f42b3758464edc44d447
Kathleen Ericson <kericson@utm.edu>
2022-08-24 16:56:59 -0500

    now with more features!

--- main.cpp
+++ main.cpp
@@ -2,9 +2,21 @@
 #include <iostream>
 
 using namespace std;
+int sum(int n);
 
 int main() {
-  cout << "Hello World!" << endl;
+  cout << "Give me number: ";
+  int n;
+  cin >> n;
+  cout << "sum: " << sum(n) << endl;
   
   return 0;
+}
+
+int sum (int n) {
+  int val = 0;
+  for (int i = 0; i < n; i++)
+    val += i;
+  val += n;
+  return val;
 }
